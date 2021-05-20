30987a637b8cf64c2ddbf358d10139fcaec599d9
deepa45514 <deepa45514@gmail.com>
2021-05-20 10:58:47 +0530

    new project added

---
+++ TestGit/src/pakage1/First.java
@@ -0,0 +1,7 @@
+package pakage1;
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("this is first package");
+	}
+}
---
+++ TestGit/src/pakage2/First111.java
@@ -0,0 +1,8 @@
+package pakage2;
+
+public class First111 {
+
+	public static void main(String[] args) {
+		System.out.println("this is second package");
+	}
+}
